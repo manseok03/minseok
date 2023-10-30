02cd3ba22beb63ae62377306ba306212017f29c1
unknown <choming0919@gmail.com>
2023-10-30 17:49:35 +0900

    minseok commit

--- 9.py
+++ 9.py
@@ -1,72 +1,3 @@
-import socket
-from select import select
-import time
-
-HOST = "127.0.0.1"
-#HOST = "localhost"
-PORT = 8080
-
-BUFSIZE = 1024
-ADDR = (HOST,PORT)
-
-"""
-#1.소켓 생성.
-srvSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#2.주소 정보 할당
-srvSock.bind(ADDR) 
-print('bind')
-
-#3.수신 대기 상태
-srvSock.listen(100) 
-print('listen')
-
-#4.연결수락
-cltSocket, addr_info = srvSock.accept() 
-print('accept')
-
-time.sleep(10)
-
-#연결 종료
-cltSocket.close() 
-
-#소켓 종료
-srvSock.close()
-
-print('close')"""
-
-# 채팅 프로그래밍
-
-"""
-srvSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srvSock.bind(('', PORT))
-srvSock.listen()
-
-cltSocket, addr = srvSock.accept()
-
-while True:
-    response = cltSocket.recv(1024)
-    print ('client : ', response.decode('utf-8'))
-
-    message = input('server : ')
-    cltSocket.send(message.encode('utf-8'))
-
-cltSocket.close()"""
-
-# UDP
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((HOST, PORT))
-
-while True:
-    data, addr = sock.recvfrom(1024)
-    print("msg: ", data)
-
-    rsp = data.decode("utf-8").upper()
-    sock.sendto(rsp.encode("utf-8"), addr)
-
-sock.close()
-
 # 속성 출력
 """
 class Person :
@@ -120,7 +51,6 @@ print(p2.name)"""
 
 # 클래스 변수
 
-"""
 class Person :
 	count = 0
 	
@@ -142,4 +72,4 @@ print(p2.count)
 
 print(p.count)
 print(p1.count)
-print(p2.count)"""
+print(p2.count)
